a20cf411e483388b0f5c87291e6458ac4b2b35b9
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2021-03-04 12:04:49 +0530

    Update 3.4 Impl Queue via Stacks.java

--- CTCI/Stack and Queue/3.4 Impl Queue via Stacks.java
+++ CTCI/Stack and Queue/3.4 Impl Queue via Stacks.java
@@ -28,28 +28,46 @@ class StackQueue
 ---------------------------------------------
 
 
-class StackQueue
-{
-    Stack<Integer> s1 = new Stack<Integer>();
-    Stack<Integer> s2 = new Stack<Integer>();
+class MyQueue {
 
-    void Push(int x)
-    {
-        s1.push(x);
+
+    Stack<Integer> s1 = new Stack();
+    Stack<Integer> s2 = new Stack();
+    int front;
+    public MyQueue() {
+        
     }
-    int Pop(){
+    
+    public void push(int x) {
+        
+        front = s1.isEmpty() ? x: front;
+        s1.push(x);
+        
+    }
+    
+    public int pop() {
+        
         
         if(s1.isEmpty() && s2.isEmpty()) {
             return -1;
         }
         
         if(s2.isEmpty()){
-            
-            while(!s1.isEmpty())
+            while(!s1.isEmpty()){
                 s2.push(s1.pop());
+            }
         }
         
         return s2.pop();
+    }
+    
+    public int peek() {
         
+        return !s2.isEmpty()  ? s2.peek() : front;
+        
+    }
+    
+    public boolean empty() {
+        return s1.isEmpty() && s2.isEmpty();
     }
 }
