cf1e75af3c53b9308696e3b65f328acbee27e897
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2021-03-14 21:12:11 +0530

    Update 4.2 Minimal Tree.java

--- CTCI/Trees and Graphs/4.2 Minimal Tree.java
+++ CTCI/Trees and Graphs/4.2 Minimal Tree.java
@@ -191,3 +191,23 @@ class Solution {
     }
     
 }
+
+//---------------------------------------
+// Below one is actually fucking easy solution which takes brain-stroming of above 3 approches 
+
+class Solution {
+    public TreeNode sortedArrayToBST(int[] array) {
+        return createMinimalBST(array, 0, array.length -1);         
+    }
+    
+ TreeNode createMinimalBST(int arr[], int start, int end) {
+     if (end < start) {
+         return null;
+     }
+     int mid = (start + end) / 2;
+     TreeNode n = new TreeNode(arr[mid]);
+     n.left = createMinimalBST(arr, start, mid - 1);
+     n.right = createMinimalBST(arr, mid + 1, end);
+     return n;
+ }
+}
