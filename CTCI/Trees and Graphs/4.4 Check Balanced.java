b573b6b5f3b7e85f0a391cf08f9573bfc62153d0
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2021-03-16 10:43:14 +0530

    Update 4.4 Check Balanced.java

--- CTCI/Trees and Graphs/4.4 Check Balanced.java
+++ CTCI/Trees and Graphs/4.4 Check Balanced.java
@@ -35,3 +35,25 @@ class Solution {
         return lefter>righter ? lefter : righter;
     }
 }
+
+//------------------------------------------------
+// Below code will perfectly work and check tree and sub tree which is balanced or not
+
+class Solution {
+    
+    public boolean isBalanced(TreeNode root) {
+        return dfs(root)!=-1;
+    }
+    
+    public int dfs(TreeNode root){
+        
+        if(root==null) return 0;
+        
+        int left  = dfs(root.left);
+        int right = dfs(root.right);
+        
+        if(left == -1 || right == -1 || Math.abs(left - right) > 1) return -1;
+        
+        return Math.max(left, right) + 1;
+    }
+}
