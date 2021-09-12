59601997bde23a950f4fc7bd90b02ec686bba59f
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2021-09-13 02:12:46 +0530

    Update 2.7 Intersection.java

--- CTCI/Linked List/2.7 Intersection.java
+++ CTCI/Linked List/2.7 Intersection.java
@@ -1,3 +1,28 @@
+//Simple version 
+
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        
+        ListNode hA = headA;
+        ListNode hB = headB;
+        
+        while(hA!=hB){
+            
+            hA = hA.next;
+            hB = hB.next;
+            
+            if(hA == null && hB != null) hA = headB;
+            if(hB == null && hA != null) hB = headA;
+            if(hA == null && hB == null) return null;
+        }
+        
+        return hA;
+    }
+}
+
+------------------------------------------------------------------------------------------
+ //Short Version
+    
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         ListNode pA = headA;
@@ -12,12 +37,7 @@ public class Solution {
     }
 }
 
-
-
-
 ------------------------------------------------------------------------------------------
-
-
 
 /**
  * Definition for singly-linked list.
