67074e4c0752656d5f4b8b780640d786f64f877b
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2022-06-02 19:29:29 -0400

    Create Coin Change.java

---
+++ DP/Coin Change.java
@@ -0,0 +1,29 @@
+class Solution {
+    public long count(int coin[], int size, int sum) {
+        
+        long matrix[][] = new long[ coin.length+1 ][ sum+1 ];
+        
+        
+        for(int row=1; row<coin.length+1; row++)
+            matrix[row][0] = 1;
+        
+        for(int col=1; col<sum+1; col++)
+            matrix[0][col] = 0;
+                
+
+        for(int i=1; i<coin.length+1; i++){
+            
+            int ValuOfCurrCoin =  coin[i-1];
+            
+            for(int j=1; j<sum+1; j++){
+                
+                if(ValuOfCurrCoin <= j){
+                   matrix[i][j] =   matrix[i-1][j] +  matrix[i][j-ValuOfCurrCoin];
+                }else{
+                   matrix[i][j] =   matrix[i-1][j];
+                }
+            }
+        }
+        return matrix[coin.length][sum];
+    }
+}
