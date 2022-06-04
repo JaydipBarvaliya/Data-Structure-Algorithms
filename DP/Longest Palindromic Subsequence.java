23382ef84c2d231d1a0dc4e418ac6ff10d388f77
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2022-06-04 03:57:20 -0400

    Create Longest Palindromic Subsequence.java

---
+++ DP/Longest Palindromic Subsequence.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+class GfG{
+    
+  public static void main(String args[]){
+      
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t-->0){
+              
+                    String s = sc.next();
+                    Solution obj = new Solution();
+                    System.out.println(obj.longestPalinSubseq(s));
+                }
+                
+        }
+}
+
+class Solution{
+    
+    public int longestPalinSubseq(String str){
+        
+        StringBuilder reverseStr = new StringBuilder();
+        reverseStr.append(str).reverse();
+        return LCS(str.toCharArray(), reverseStr.toString().toCharArray(), str.length(), reverseStr.length() );
+    }
+    
+    static int LCS(char[] X, char[] Y, int m, int n ){
+      
+        int matrix[][] = new int[ m+1 ][ n+1 ];
+    
+        for(int i=1; i<m+1; i++){
+            
+            for(int j=1; j<n+1; j++){
+                
+                if (X[i-1] == Y[j-1]){
+            	    matrix[i][j] =  1 + matrix[i-1][j-1];  
+            	}else {
+            	    matrix[i][j] =  Math.max( matrix[i][j-1], matrix[i-1][j] );
+            	}
+            }
+        }
+    	return matrix[m][n];
+      }
+}
