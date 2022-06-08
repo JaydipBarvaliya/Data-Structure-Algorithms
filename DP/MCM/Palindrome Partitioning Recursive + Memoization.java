3c105ba6e13c573fddd7d16675f6151f6bdfbd78
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2022-06-08 05:59:05 -0400

    Update Palindrome Partitioning Recursive + Memoization.java

--- DP/MCM/Palindrome Partitioning Recursive + Memoization.java
+++ DP/MCM/Palindrome Partitioning Recursive + Memoization.java
@@ -1,1 +1,41 @@
+class Solution{
+    static int palindromicPartition(String str){
+        
+        int[][] matrix = new int[str.length() + 1 ][str.length() + 1];
+        for (int[] row : matrix)
+            Arrays.fill(row, -1);
+        
+        return solve(matrix, str, 0, str.length()-1);
+    }
+    
+    static int solve(int[][] matrix, String str, int i, int j){
+        
+        
+        if( i>=j || isPalindrome(str, i, j) ) return 0;
+        if(matrix[i][j] != -1) return matrix[i][j];
 
+        
+        
+        int min = Integer.MAX_VALUE;
+        for(int k=i; k<j; k++){
+            int tempAns = 1 + solve(matrix, str, i, k) + solve(matrix, str, k+1, j);
+            min  = Math.min(min, tempAns);
+        }
+        
+        
+        matrix[i][j] = min;
+        return min;
+    }
+    
+     static boolean isPalindrome(String str, int i, int j){
+        
+        while(i < j){
+          if(str.charAt(i) != str.charAt(j))
+            return false; 
+          i++;
+          j--;
+        }
+        
+        return true;
+      }
+}
