552927af6750319b4d8b68d2999f6e1ffb0e1304
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2022-06-09 04:38:03 -0400

    Update Coin Change Minimum number of coins.java

--- DP/Knapsack/Coin Change Minimum number of coins.java
+++ DP/Knapsack/Coin Change Minimum number of coins.java
@@ -1,3 +1,9 @@
+//one reason I think that is possible to initialize the matrix my +ve 
+// infinity is because we need to find the minimum number of coins. 
+// If we put 0 which Aditya Bhaiya pointed out in the video, 
+// then calculating bigger problems by using smaller sub-problems will always give 0 because 0 is going to be the minimum in the matrix.
+// That is why we don't take 0 although it seems to be there
+
 class Solution{
 
 	public int minCoins(int coins[], int len, int sum){ 
