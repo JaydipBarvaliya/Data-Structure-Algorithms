fc498582c142d0cd0676ccb89f39636714828750
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2022-06-10 07:13:33 -0400

    Create Diameter of a Binary Tree.java

---
+++ DP/Tree/Diameter of a Binary Tree.java
@@ -0,0 +1,21 @@
+class Solution {
+    
+    int max = Integer.MIN_VALUE;
+    
+    public int diameterOfBinaryTree(TreeNode root) {
+        fun(root);
+        return max;
+    }
+    
+    public int fun(TreeNode root) {
+        
+        if(root == null) return 0;
+        
+        int left  = fun(root.left);
+        int right = fun(root.right);
+        
+        max = Math.max(max, left+right);
+        
+        return 1 + Math.max(left, right);
+    }
+}
