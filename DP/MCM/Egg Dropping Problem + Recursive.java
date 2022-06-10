53cd6f308986becf1c73807d573db25312b449b0
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2022-06-10 04:10:17 -0400

    Update Egg Dropping Problem + Recursive.java

--- DP/MCM/Egg Dropping Problem + Recursive.java
+++ DP/MCM/Egg Dropping Problem + Recursive.java
@@ -28,3 +28,22 @@
 // if it requires greater number of attempts to find the critical floor if the egg would break if dropped form the 3rd floor
 // then we would assume that upon dropping from the 3rd floor the egg would actually break because we want the worst case here.
 // Then we take the minimum of number of attempts required for finding the critical floor in worst case of all of the floors in our range and that is the answer.
+
+
+
+class Solution {
+    
+    static int eggDrop(int e, int f){
+        
+        if(e==0 || f==0 || e==1) return f;
+        
+        int min = Integer.MAX_VALUE;
+        
+        for(int k=1; k<=f; k++){
+            
+            int tempAns = 1 + Math.max(eggDrop(e-1, k-1), eggDrop(e, f-k));
+            min = Math.min(min, tempAns);
+        }
+        return min;
+	}
+}
