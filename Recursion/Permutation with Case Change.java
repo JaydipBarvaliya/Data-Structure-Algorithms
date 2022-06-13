12607e1b1f60e2064ea7d233554425e0b2477558
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2022-06-13 08:38:00 -0400

    Create Permutation with Case Change.java

---
+++ Recursion/Permutation with Case Change.java
@@ -0,0 +1,25 @@
+class Solution {
+    
+    TreeSet<String> set = new TreeSet<String>();
+    
+    public List<String> letterCasePermutation(String str) {
+        
+        String op = "";
+        solve(str, op);
+        return new ArrayList<String>(set);
+    }
+    
+        void solve(String ip, String op){
+        
+        if(ip.length() == 0){
+            set.add(op);
+            return;
+        }
+        
+        solve(ip.substring(1), op + Character.toUpperCase(ip.charAt(0)));
+        solve(ip.substring(1), op + Character.toLowerCase(ip.charAt(0)));
+        
+        //Must remeber that ip.substring(1) must execute after ip.charAt(0) 
+        //otherwise null pointer can happen for charAt Method
+}
+}
