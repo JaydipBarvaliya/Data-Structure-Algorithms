c9d074f9152f9ce61d0503100f6c51c32169bb97
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2022-06-13 04:14:58 -0400

    Update Subsets.java

--- Recursion/Subsets.java
+++ Recursion/Subsets.java
@@ -1,5 +1,13 @@
 class Solution {
-    static void getSS(int arr[], int idx, List<Integer> comb, List<List<Integer>> list){
+    
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        solve(nums, 0, new ArrayList<>(), list);
+        return list;
+    }
+    
+    
+    void solve(int arr[], int idx, List<Integer> comb, List<List<Integer>> list){
         
         if(idx == arr.length){
             list.add(new ArrayList<>(comb));
@@ -7,22 +15,11 @@ class Solution {
         }
         
         comb.add(arr[idx]);
-        getSS(arr, idx + 1, comb, list);
-        
+        solve(arr, idx + 1, comb, list);
         
         comb.remove(comb.size()-1);
-        getSS(arr, idx + 1, comb, list);
+        solve(arr, idx + 1, comb, list);
         
         // The above statement are specially defined in hirerchy because of pass by refernece issue for "comb".
-        
-        
-    }
-    
-    public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> list = new ArrayList<>();
-        
-        getSS(nums, 0, new ArrayList<>(), list);
-            
-        return list;
     }
 }
