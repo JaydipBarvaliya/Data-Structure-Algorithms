5323e97d1db26ee1ec3e0eff681b906ef7b203b6
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2022-06-18 02:32:59 -0400

    Update First Negative Number in every Window of Size K.java

--- Sliding Window/First Negative Number in every Window of Size K.java
+++ Sliding Window/First Negative Number in every Window of Size K.java
@@ -1,32 +1,35 @@
-public class Main {
-    public static void main(String[] args) {
+class Compute {
+    
+    public long[] printFirstNegativeInteger(long arr[], int n, int k){
         
-        int[] arr = new int[]{12, -1, -7, 8, -15, 30, 16, 28};
-        ArrayList<Integer> nagArr = new ArrayList();
-        ArrayList<Integer> output = new ArrayList();
-        int i,j,k;
-            i=j=0;
-            k=3;
         
-        while(j<arr.length){
-
-            if(arr[j]<0){
-                nagArr.add(arr[j]);
+        long[] result = new long[n-k+1];
+        List<Long> nagList = new ArrayList<Long>();
+        int i = 0;
+        int j = 0;
+        int index = 0;
+        
+        while(j<n){
+            
+            if(arr[j]<0) nagList.add(arr[j]);
+            
+            if(j-i+1<k){
+                j++;
+            }else{
+                
+                if(nagList.size() == 0){
+                    result[index] = 0;
+                }else{
+                    result[index] = nagList.get(0);
+                    if(nagList.get(0) == arr[i]) nagList.remove(0);
+                }
+                
+                i++;
+                j++;
+                index++;
             }
             
-            if(j-i+1 == k){
-                if(nagArr.size() == 0){
-                 output.add(0);
-                }else{
-                 output.add(nagArr.get(0));
-                    if(arr[i] == nagArr.get(0)){
-                        nagArr.remove(0);
-                    }
-                }
-                i++;
-            }
-            j++;
         }
-        System.out.println(output);
+        return result;
     }
 }
