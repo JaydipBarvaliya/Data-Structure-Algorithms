571e04bada38f8cadc5ff3e04c28c36ee6571590
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2022-06-19 07:12:22 -0400

    Create Longest Sub-Array with Sum K  + Positive Integer Only.java

---
+++ Sliding Window/Latest/Longest Sub-Array with Sum K  + Positive Integer Only.java
@@ -0,0 +1,35 @@
+class Solution{
+    
+    public static int lenOfLongSubarr (int arr[], int n, int windowSum) {
+        
+        int i=0;
+        int j=0;
+        
+        int max = Integer.MIN_VALUE;
+        int sum=0;
+        while(j<n){
+            
+            sum = sum+ arr[j];
+            
+            if(sum < windowSum){
+                j++;
+            }else if(sum == windowSum){
+                max = Math.max(j-i+1, max);
+                j++;
+            }else if(sum > windowSum){
+                
+                while(sum>windowSum && i<=j){
+                    sum = sum - arr[i];
+                    i++;
+                }
+                
+                if(sum == windowSum){
+                  max = Math.max(j-i+1, max);
+                }
+                j++;
+            }
+        }
+        
+        return max;
+    }
+}
