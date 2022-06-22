9d9a76758e79bc8b6d35f11628a74c9eb2e3db16
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2022-06-22 07:52:47 -0400

    Create Next Alphabetical Element.java

---
+++ Binary Search/Next Alphabetical Element.java
@@ -0,0 +1,28 @@
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+      
+        int len = letters.length;
+        int start = 0;
+        int end   = len-1;
+        int result   = -1;
+        
+        while(start<=end){
+           
+           int mid = start + (end-start)/2;
+           
+           if(letters[mid] == target){
+               result = (mid + 1) % len;
+                start = mid + 1;
+           }
+           else if(target < letters[mid]){
+               result = mid;
+               end = mid - 1;
+           }   
+           else if(target > letters[mid])
+               start = mid + 1;
+       }
+        
+        if(result == -1 ) return letters[0];
+        else return letters[result];
+    }
+}
