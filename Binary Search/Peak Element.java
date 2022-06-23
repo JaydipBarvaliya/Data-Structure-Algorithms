d8e00f71c544877b9859d1baedbf4ae7d5549a03
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2022-06-23 05:18:16 -0400

    Update Peak Element.java

--- Binary Search/Peak Element.java
+++ Binary Search/Peak Element.java
@@ -1,43 +1,31 @@
-class Solution{
-	
-	public int peakElement(int[] arr, int len){
-	    
-	    if(len==1) return 1;
+class Solution {
+    public int findPeakElement(int[] arr) {
+        
+        int len = arr.length;        
 	    int start  = 0;
 	    int end    = len-1;
-	    int result = 0;
+	    int result = 1;
+        
+        if(len==1) return 0;
 	    
-	    while(start<=end){
+	    while(start <= end){
            
            int mid = start + (end-start)/2;
            
            if(mid>0 && mid<len-1){
 
-               if(arr[mid] > arr[mid+1] && arr[mid] > arr[mid-1]){
-                   return mid;
-               }else if(arr[mid+1]> arr[mid]){
-                   start = mid+1;
-               }else if(arr[mid-1] > arr[mid]){
-                   end = mid-1;
-               }
+                if(arr[mid] > arr[mid+1] && arr[mid] > arr[mid-1]) return mid;
+                else if(arr[mid+1] > arr[mid]) start = mid+1;
+                else  end = mid-1;
+              
 
            }else if(mid == 0){
-
-               if(arr[0] > arr[1]){
-                   return 0;
-               }else{
-                   return 1;
-               }
-
+               return arr[0] > arr[1] ? 0 : 1;
+               
            }else if(mid == len-1) {
-               if(arr[len-1] > arr[len-2]){
-                   return len-1;
-               }else{
-                   return len-2;
-               }
+               return arr[len-1] > arr[len-2] ? len-1 : len-2;
            }
 	    }
-	    
 	    return result;
     }
 }
