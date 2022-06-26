abf9517509edff30df80764a6b8303bcb1bc4871
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2022-06-26 19:05:15 -0400

    Create Detect cycle in an undirected graph + Optimized.java

---
+++ Graph/Detect cycle in an undirected graph + Optimized.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean isCycle(int V, ArrayList<ArrayList<Integer>> list) {
+        
+      boolean[] visited = new boolean[V];
+      
+      Arrays.fill(visited, false);
+      
+      for(int i=0; i<V; i++){
+          if(visited[i] == false){
+              if(detectCycle(i, -1, list, visited) == true)
+                  return true;
+          }
+      }
+      
+      return false;
+  }
+    
+    static boolean detectCycle(int node, int parent, ArrayList<ArrayList<Integer>> list, boolean[] visited){
+
+        visited[node] = true;
+        
+        for(Integer neigh : list.get(node)){
+            
+            if(visited[neigh] == false){
+                if(detectCycle(neigh, node, list, visited) == true)
+                    return true;
+            }
+            else if(neigh != parent)
+                 return true;
+        }
+
+        return false;
+   }
+    
+}
