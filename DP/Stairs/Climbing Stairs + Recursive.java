e4c00f6f9616f41cef035ccb6410a694045a3fd0
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2022-06-30 14:19:56 -0400

    Create Climbing Stairs + Recursive.java

---
+++ DP/Stairs/Climbing Stairs + Recursive.java
@@ -0,0 +1,12 @@
+class Solution {
+    
+        int climbStairs(int n) {
+            return solve(n);
+        }
+        
+    
+        int solve(int n){
+            if(n < 2) return 1;
+            return solve(n - 1) + solve(n-2);
+        }
+};
