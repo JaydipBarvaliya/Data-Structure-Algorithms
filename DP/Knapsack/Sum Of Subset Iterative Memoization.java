bd0e7570eb6f0ebdc62383582123a0841f66668c
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2022-08-07 08:29:50 -0400

    Update Sum Of Subset Iterative Memoization.java

--- DP/Knapsack/Sum Of Subset Iterative Memoization.java
+++ DP/Knapsack/Sum Of Subset Iterative Memoization.java
@@ -1,37 +1,3 @@
-// { Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class GFG
-{
-    public static void main(String args[])throws IOException
-    {
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine());
-        while(t-- > 0)
-        {
-            int N = Integer.parseInt(read.readLine());
-            String input_line[] = read.readLine().trim().split("\\s+");
-            int arr[]= new int[N];
-            for(int i = 0; i < N; i++)
-                arr[i] = Integer.parseInt(input_line[i]);
-            int sum = Integer.parseInt(read.readLine());
-
-            Solution ob = new Solution();
-            if(ob.isSubsetSum(N, arr, sum))
-            System.out.println(1);
-            else
-            System.out.println(0);
-
-            
-        }
-    }
-}
-// } Driver Code Ends
-
-
-//User function Template for Java
-
 class Solution{
 
 
