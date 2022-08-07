3990c947e42578e59e746717c87257c76540682d
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2022-08-07 10:22:38 -0400

    Update Count of Subset Sum Iterative + Memoization.java

--- DP/Knapsack/Count of Subset Sum Iterative + Memoization.java
+++ DP/Knapsack/Count of Subset Sum Iterative + Memoization.java
@@ -1,29 +1,3 @@
-// { Driver Code Starts
-//Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-class GfG
-{
-    public static void main(String args[])
-        {
-            Scanner sc = new Scanner(System.in);
-            int t = sc.nextInt();
-            while(t-->0)
-                {
-                    int n = sc.nextInt();
-                    int sum = sc.nextInt();
-                    int arr[] = new int[n];
-                    for(int i = 0;i<n;i++)
-                    arr[i] = sc.nextInt();
-                    
-                    Solution ob = new Solution();
-                    System.out.println(ob.perfectSum(arr,n,sum));
-                }
-        }
-}    // } Driver Code Ends
-
-
 class Solution{
 
 	public int perfectSum(int items[],int n, int totalSum) { 
