ab2b802d36392c8ecc1b9f6de6e54fb261dce1c2
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2022-08-07 09:41:27 -0400

    Update Equal Sum Partition.java

--- DP/Knapsack/Equal Sum Partition.java
+++ DP/Knapsack/Equal Sum Partition.java
@@ -1,29 +1,3 @@
-import java.io.*;
-import java.util.*;
-
-class GFG{
-    public static void main(String args[])throws IOException
-    {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(in.readLine());
-        while(t-- > 0){
-            int N = Integer.parseInt(in.readLine());
-            String input_line[] = in.readLine().trim().split("\\s+");
-            int arr[] = new int[N];
-            for(int i = 0;i < N;i++)
-                arr[i] = Integer.parseInt(input_line[i]);
-            
-            Solution ob = new Solution();
-            int x = ob.equalPartition(N, arr);
-            if(x == 1)
-                System.out.println("YES");
-            else
-                System.out.println("NO");
-        }
-    }
-}
-
-
 class Solution{
     static int equalPartition(int len , int items[]){
         
