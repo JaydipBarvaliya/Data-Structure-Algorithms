236a395680f0506767d2da0a1407cefb1a378dd4
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2022-08-08 18:16:24 -0400

    Update Count of Subset Sum Iterative + Memoization.java

--- DP/Knapsack/Count of Subset Sum Iterative + Memoization.java
+++ DP/Knapsack/Count of Subset Sum Iterative + Memoization.java
@@ -1,43 +1,43 @@
-class Solution{
+class Solution {
 
-	public int perfectSum(int items[],int n, int totalSum) { 
-	    
-	    int matrix[][] = new int[items.length + 1][totalSum + 1];
+    public int perfectSum(int items[], int n, int totalSum) {
 
-		for (int col=1;  col<=totalSum;  col++)
-			matrix[0][col] = 0;
-        
+        int matrix[][] = new int[items.length + 1][totalSum + 1];
+
+        for (int col = 1; col <= totalSum; col++)
+            matrix[0][col] = 0;
+
 
         matrix[0][0] = 1;
         int zeroCount = 0;
-        
-		for (int row=1;  row<items.length+1;  row++){
-	        if(items[row-1] == 0) { 
-	            zeroCount++;
-	            matrix[row][0] = (int)Math.pow(2, zeroCount);
-	        }else{
-	            matrix[row][0] = (int)Math.pow(2, zeroCount);
-	        }
-    	}
-    			 
 
-		for (int i = 1; i <= items.length; i++) {
-			
-			int currentItem  = items[i-1];
-			
-			for (int j = 1; j <= totalSum; j++) {
-			    if(currentItem <= j){
-			        matrix[i][j] = matrix[i-1][j] + matrix[i-1] [j - currentItem];
-			    }else{
-			    	matrix[i][j] = matrix[i-1][j];			        
-			    }
+        for (int row = 1; row < items.length + 1; row++) {
+            if (items[row - 1] == 0) {
+                zeroCount++;
+                matrix[row][0] = (int) Math.pow(2, zeroCount);
+            } else {
+                matrix[row][0] = (int) Math.pow(2, zeroCount);
             }
-		}
+        }
 
 
-		for (int[] row : matrix)
+        for (int i = 1; i <= items.length; i++) {
+
+            int currentItem = items[i - 1];
+
+            for (int j = 1; j <= totalSum; j++) {
+                if (currentItem <= j) {
+                    matrix[i][j] = matrix[i - 1][j] + matrix[i - 1][j - currentItem];
+                } else {
+                    matrix[i][j] = matrix[i - 1][j];
+                }
+            }
+        }
+
+
+        for (int[] row: matrix)
             System.out.println(Arrays.toString(row));
 
-		return matrix[items.length][totalSum];
-	} 
+        return matrix[items.length][totalSum];
+    }
 }
