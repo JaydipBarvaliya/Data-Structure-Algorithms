e2a3862efa268b7878daef641d1e49da3f03fe2a
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2022-08-08 19:23:15 -0400

    Update Minimum Subset Sum Difference Recursion.java

--- DP/Knapsack/Minimum Subset Sum Difference Recursion.java
+++ DP/Knapsack/Minimum Subset Sum Difference Recursion.java
@@ -1,28 +1,3 @@
-// { Driver Code Starts
-//Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-class GfG
-{
-    public static void main(String args[])
-        {
-            Scanner sc = new Scanner(System.in);
-            int t = sc.nextInt();
-            while(t-->0)
-                {
-                    int n = sc.nextInt();
-                    int A[] = new int[n];
-                    for(int i = 0;i<n;i++)
-                        A[i] = sc.nextInt();
-                    Solution ob = new Solution();
-                    System.out.println(ob.minDifference(A,n));
-                }
-        }
-}    // } Driver Code Ends
-
-
-
 class Solution{
 
     public int minDifference(int arr[], int len) {
