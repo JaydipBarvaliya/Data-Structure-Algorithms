8f1c2248fbe0c84fc98b286f8a2f5e5fa1251856
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2022-08-14 17:41:34 -0400

    Update 10 Minimum difference element in a sorted array.java

--- Binary Search/10 Minimum difference element in a sorted array.java
+++ Binary Search/10 Minimum difference element in a sorted array.java
@@ -6,13 +6,13 @@
 //to the our main key element 
 
 
-	while(start<=end){
-		
-            int mid = start + (end - start)/2;
-			
-            if (arr[mid] == key) return arr[mid];
-            else if (arr[mid] > key) end = mid-1;
-			else if (arr[mid] < key) end = mid+1;
+  while (start <= end) {
+
+	  int mid = start + (end - start) / 2;
+
+	  if (arr[mid] == key) return arr[mid];
+	  else if (arr[mid] > key) end = mid - 1;
+	  else if (arr[mid] < key) end = mid + 1;
 	}
-	
-	return (mid-start < mid-end)? start :end;
+
+ return (mid - start < mid - end) ? start : end;
