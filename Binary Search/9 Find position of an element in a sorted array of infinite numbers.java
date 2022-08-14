102c09e3ca16567cb4fa60320b3091873c5033ba
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2022-08-14 17:10:08 -0400

    Update 9 Find position of an element in a sorted array of infinite numbers.java

--- Binary Search/9 Find position of an element in a sorted array of infinite numbers.java
+++ Binary Search/9 Find position of an element in a sorted array of infinite numbers.java
@@ -1,48 +1,51 @@
 class Test{
    
-    static int binarySearch(int arr[], int l, int r, int x)
-    {
-        if (r>=l)
-        {
+    static int binarySearch(int arr[], int l, int r, int x){
+	    
+	    
+        if (r>=l){
+
             int mid = l + (r - l)/2;
+
             if (arr[mid] == x)
                 return mid;
-            if (arr[mid] > x)
+
+            if (x < arr[mid])
                 return binarySearch(arr, l, mid-1, x);
-				
-            return binarySearch(arr, mid+1, r, x);
+
+	    if (x > arr[mid])
+		return binarySearch(arr, mid+1, r, x);
         }
         return -1;
     }
-	
+
     static int findPos(int arr[],int key){
         int low = 0, high = 1;
         int val = arr[0];
- 
-        
+
         while (val < key){
+
             low = high;     // store previous high
-            //check that 2*h doesn't exceeds array
-            //length to prevent ArrayOutOfBoundException
-            if(2*high < arr.length-1)
-               high = 2*high;
+
+	    //check that 2*h doesn't exceeds array length to prevent ArrayOutOfBoundException
+            if(2*high < arr.length-1)		    
+	        high = 2*high;
             else
-               high = arr.length-1;
-             
+                high = arr.length-1;
+	    		
             val = arr[high]; // update new val
         }
  
-        // at this point we have updated low
-        // and high indices, thus use binary
-        // search between them
+        // at this point we have updated low and high indices, thus use binary search between them
         return binarySearch(arr, l, h, key);
     }
 	
+	
+	
     public static void main(String[] args){
-        int arr[] = new int[]{3, 5, 7, 9, 10, 90,
-                            100, 130, 140, 160, 170};
-        int ans = findPos(arr,10);
-		
+        int arr[] = new int[]{3, 5, 7, 9, 10, 90, 100, 130, 140, 160, 170};
+        
+	int ans = findPos(arr,10);	
         if (ans==-1)
             System.out.println("Element not found");
         else
