78797205d758456abfe7fc94650994bb2ce0558b
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2022-08-14 17:55:01 -0400

    Update 11 Minimum difference element in a sorted array.java

--- Binary Search/11 Minimum difference element in a sorted array.java
+++ Binary Search/11 Minimum difference element in a sorted array.java
@@ -1,5 +1,6 @@
 //This problem can be solved in another way like, find floor and ceil of target and then find the minimum absolute differnce of them and return it.
-//In this below exam what we did....  if the target is present in the array then that is the element which generate minimum diff.
+
+//In this below example what we did....  if the target is present in the array then that is the element which generate minimum diff.
 //If that is not present then we can say that the two elements which are nearer or both the side of elements in the array would be the candiate for the min 
 //diff elements. so we can find those elements and then find the answers....so if we go for the Binary search then if key is not present in the array then
 //our loop will stop at the place where our start and end cross each other and co-incidently that will be the ceil/floor or the two candiates who are near 
