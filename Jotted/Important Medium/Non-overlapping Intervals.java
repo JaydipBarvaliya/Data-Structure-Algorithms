f8b397129da3609558bfcae7aac21f5a44222777
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2022-08-15 20:44:44 -0400

    Update Non-overlapping Intervals.java

--- Jotted/Important Medium/Non-overlapping Intervals.java
+++ Jotted/Important Medium/Non-overlapping Intervals.java
@@ -1,14 +1,17 @@
 class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
+        
         Arrays.sort(intervals,(a,b)->Integer.compare(a[1],b[1]));
         
-        int []p = intervals[0];
+        int[] curr = intervals[0];    
         int count = 0;
+        
         for(int i=1;i<intervals.length;i++){
-            if (p[1] > intervals[i][0])
-                count+=1;
+            
+            if (curr[1] > intervals[i][0])
+                count++;
             else
-                p = intervals[i];
+                curr = intervals[i];
         }
         
         return count;
