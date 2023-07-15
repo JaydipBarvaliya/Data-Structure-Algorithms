c71e3f085db0285f7a42db4421a2a4be055da00d
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2023-07-15 17:10:15 -0400

    REFACTOR

--- NoteBook/DP/Knapsack/0-1/Subset Sum + Iterative Memoization.java
+++ NoteBook/DP/Knapsack/0-1/Subset Sum + Iterative Memoization.java
@@ -1,28 +1,31 @@
+import java.util.Arrays;
+
 class Solution{
 	
-    static Boolean isSubsetSum(int N, int items[], int totalSum){
-     boolean matrix[][] = new boolean[items.length + 1][totalSum + 1];
+    static Boolean isSubsetSum(int N, int arr[], int totalSum){
+
+		boolean[][] matrix;
+		matrix = new boolean[arr.length + 1][totalSum + 1];
 
 		for (int col=0;  col<=totalSum;  col++)
 			matrix[0][col] = false;
 
-		for (int row=0;  row<items.length+1;  row++)
+		for (int row=0;  row<arr.length+1;  row++)
 			matrix[row][0] = true;
 
 
-		for (int i = 1; i <= items.length; i++) {
+		for (int i = 1; i <= arr.length; i++) {
 			
-			int currentItem  = items[i-1];
 			for (int j = 1; j <= totalSum; j++) {
-			    if(currentItem <= j){
-			        matrix[i][j] = matrix[i-1][j] || matrix[i-1] [j - currentItem];
+			    if(arr[i-1] <= j){
+			        matrix[i][j] = matrix[i-1][j] || matrix[i-1] [j - arr[i-1]];
 			    }else{
 			    	matrix[i][j] = matrix[i-1][j];			        
 			    }
             }
 		}
-		for (int[] row : matrix)
+		for (boolean[] row : matrix)
             		System.out.println(Arrays.toString(row));
-		return matrix[items.length][totalSum];
+		return matrix[arr.length][totalSum];
     }
 }
--- NoteBook/DP/Knapsack/0-1/Subset Sum + Recursive.java
+++ NoteBook/DP/Knapsack/0-1/Subset Sum + Recursive.java
@@ -6,14 +6,16 @@ class Solution{
         if (sum == 0) return true;
         if (n   == 0) return false;
         
-        // If last element is greater than
-        // sum, then ignore it
+        // If last element is greater than sum, then ignore it
+
         if (arr[n - 1] <= sum){
             
             /* Check if sum can be obtained by any of the following
             (a) including the last element
             (b) excluding the last element */
-            return isSubsetSum(n-1, arr, sum-arr[n-1]) || isSubsetSum(n-1, arr, sum);
+            return isSubsetSum(n-1, arr, sum-arr[n-1])
+                    ||
+                   isSubsetSum(n-1, arr, sum);
         }
         else{
             return isSubsetSum(n-1, arr, sum);
