4b72216e0fcc6536cbae89b22862ea53e3c6ae40
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2023-07-29 14:36:52 -0400

    Sliding Window Started.

--- DONE/Sliding Window/1 Maximum Sum of Subarray of size K.java
+++ DONE/Sliding Window/1 Maximum Sum of Subarray of size K.java
@@ -4,8 +4,11 @@ class Solution{
     long max=0;
     int i=0;
     
-    for(i=0; i<k; i++) max = max + list.get(i);
+    for(i=0; i<k; i++){
+         max = max + list.get(i);
+    }
     
+    //This assignment is very importnant as max and sumOfPrevWin both aim is different. 
     long sumOfPreWin = max;
     
     while(i<n){
