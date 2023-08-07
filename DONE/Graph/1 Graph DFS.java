33f7c258880c9486b40de06659230e2a58392982
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2023-08-07 00:01:38 -0400

    Do not know why I'm committing.

--- DONE/Graph/1 Graph DFS.java
+++ DONE/Graph/1 Graph DFS.java
@@ -1,5 +1,3 @@
-
-
 // Time Complexity: For an undirected graph, O(N) + O(2E), For a directed graph, O(N) + O(E), Because for every node we are calling the recursive function once, the time taken is O(N) and 2E is for total degrees as we traverse for all adjacent nodes.
 // Space Complexity: O(3N) ~ O(N), Space for dfs stack space, visited array and an adjacency list.
 
--- DONE/Graph/2 Graph BFS.java
+++ DONE/Graph/2 Graph BFS.java
@@ -1,21 +1,25 @@
+// Time Complexity: O(N) + O(E), Where N = Nodes, E is for total degrees as we traverse all adjacent nodes. [ For undirected it is O(2E) ]
+// Space Complexity: O(3N) ~ O(N), Space for queue data structure visited array and an adjacency list
+
+
 class Solution {
     public ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
         
         ArrayList<Integer> result = new ArrayList<>();
-        boolean visited[] = new boolean[V];
+        boolean[] visited = new boolean[V];
         Queue<Integer> queue = new LinkedList<>();
-
+        
         queue.add(0);
         visited[0] = true;
         
         while(!queue.isEmpty()){
             
-            Integer node = queue.poll();
+            int curr = queue.poll();
+            result.add(curr);
             
-            result.add(node);
-            
-            for(Integer neigh : adj.get(node)){
-                if(visited[neigh] == false){
+            for(int neigh : adj.get(curr)){
+                
+                if(!visited[neigh]){
                     visited[neigh] = true;
                     queue.add(neigh);
                 }
