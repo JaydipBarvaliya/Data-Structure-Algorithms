5e246a48095686f7de4b3e760bfa1dd6543866e4
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2023-09-16 21:45:38 -0400

    Egg Dropping Problem Added.

---
+++ DONE/MCM/13 Egg Dropping Problem + Recursive + Memoization.java
@@ -0,0 +1,55 @@
+import java.util.Arrays;
+
+
+
+class Solution {
+    public int superEggDrop(int e, int f) {
+
+        int[][] matrix = new int[e+1][f+1];
+        for(int[] row : matrix)
+            Arrays.fill(row, -1);
+
+        return solve(e, f, matrix);
+    }
+
+    public int solve(int e, int f, int[][] matrix){
+
+        if(e==1) return f;
+        if(f==0) return 0;
+        if(f==1) return 1;
+
+        if(matrix[e][f] != -1) return matrix[e][f];
+
+        int min = Integer.MAX_VALUE;
+
+
+        //Remember this is Linear Solution where we are going from left to right, next solution will be the binary one, where
+        // we break the middle part and then either left or right. So it will help us to store maximum answer in minimum attempt in matrix
+        // If we can quickly fill the matrix then we do not need to go deeper as most of the answers are available in matrix and binary search will do the same
+        
+        for(int k=1; k<=f; k++){
+                
+                
+                int eggBreak=0;
+                if(matrix[e-1][k-1] != -1){
+                    eggBreak = matrix[e-1][k-1];
+                }else{
+                    eggBreak = solve(e-1, k-1, matrix);
+                }
+
+
+                int eggDontBreak=0;
+                if(matrix[e-1][k-1] != -1){
+                    eggDontBreak = matrix[e][f-k];
+                }else{
+                    eggDontBreak = solve(e, f-k, matrix);
+                }
+
+                int maxAttemptsInWorstCase = 1 + Math.max(eggBreak, eggDontBreak);
+                min = Math.min(min, maxAttemptsInWorstCase);
+        }
+
+        matrix[e][f] = min;
+        return min;
+    }
+}
---
+++ DONE/MCM/14 Egg Dropping Problem + Recursive + Memoization + Optimized.java
@@ -0,0 +1,62 @@
+import java.util.Arrays;
+
+class Solution {
+    public int superEggDrop(int e, int f) {
+
+        int[][] matrix = new int[e+1][f+1];
+        for(int[] row : matrix)
+            Arrays.fill(row, -1);
+
+        return solve(e, f, matrix);
+    }
+
+    public int solve(int e, int f, int[][] matrix){
+
+        if(e==1) return f;
+        if(f==0) return 0;
+        if(f==1) return 1;
+
+        if(matrix[e][f] != -1) return matrix[e][f];
+
+        int min = Integer.MAX_VALUE;
+        int low = 0;
+        int high = f;
+
+        while(low <= high){
+
+            int mid = (low + high)/2;
+            int lowerFloors  = 0;
+            int higherFloors = 0;
+            if(matrix[e-1][mid-1] != -1){
+                lowerFloors = matrix[e-1][mid-1];
+            }else{
+                lowerFloors =  solve(e-1, mid-1, matrix);
+                matrix[e-1][mid-1] = lowerFloors;
+            }
+
+            if(matrix[e][f-mid] != -1){
+                higherFloors = matrix[e][f-mid];
+            }else{
+                higherFloors =  solve(e, f-mid, matrix);
+                matrix[e][f-mid]   = higherFloors;
+            }
+
+            int temp = 1 + Math.max(lowerFloors, higherFloors);
+
+
+            //We are dividing the building with mid to upper floors and mid to lowerfloors. and then our solve function find out the
+            //attempts both sides (upper & lower). Now out of both the answers we need to stick with the worst case which means the whichever
+            //has higher value that part has maximum numbers of attempts done and based on that we are considering either upper floors or lower
+            // floor for next calulcation and based on that we are moving our low and high.
+            if(lowerFloors < higherFloors){
+                low = mid+1;
+            }else{
+                high = mid-1;
+            }
+            min = Math.min(min, temp);
+        }
+
+        matrix[e][f] = min;
+        return min;
+    }
+}
